c3a1b954847594bd1fc0548c9a41f8ae0681dabf
BuNuo <1434244213@qq.com>
2018-05-03 22:22:38 +0800

    init

---
+++ src/components/three.vue
@@ -0,0 +1,21 @@
+<template>
+	<section>
+		<p class="message">33333333333333333333333333</p>
+	</section>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+			}
+		}
+	}
+
+</script>
+
+<style scoped>
+	.message{
+		font-size: 18
+	}
+</style>
